b0b9324d2c7dd153c5499c07905b9462e0b32a39
Dick Hamill <36781010+MrEkted@users.noreply.github.com>
2023-04-19 23:11:48 -0400

    Cleaned up serial port definitions and debug mode

--- MPF-LISY-Endpoint.ino
+++ MPF-LISY-Endpoint.ino
@@ -13,8 +13,9 @@
 
 #define GAME_MAJOR_VERSION  2023
 #define GAME_MINOR_VERSION  1
-#define DEBUG_MESSAGES  1
+#define DEBUG_MESSAGES  0
 
+#define DebugSerial Serial
 #define LISYSerial Serial2
 
 
@@ -30,8 +31,8 @@ void setup() {
 
   if (DEBUG_MESSAGES) {
     // If debug is on, set up the Serial port for communication
-    Serial.begin(115200);
-    Serial.write("Starting\n");
+    DebugSerial.begin(115200);
+    DebugSerial.write("Starting\n");
   }
 
   CurrentTime = millis();
@@ -47,14 +48,14 @@ void setup() {
   if (DEBUG_MESSAGES) {
     char buf[128];
     sprintf(buf, "Return from init = 0x%04lX\n", initResult);
-    Serial.write(buf);
-    if (initResult&RPU_RET_6800_DETECTED) Serial.write("Detected 6800 clock\n");
-    else if (initResult&RPU_RET_6802_OR_8_DETECTED) Serial.write("Detected 6802/8 clock\n");
-    Serial.write("Back from init\n");    
+    DebugSerial.write(buf);
+    if (initResult&RPU_RET_6800_DETECTED) DebugSerial.write("Detected 6800 clock\n");
+    else if (initResult&RPU_RET_6802_OR_8_DETECTED) DebugSerial.write("Detected 6802/8 clock\n");
+    DebugSerial.write("Back from init\n");    
   }
   if (initResult&RPU_RET_ORIGINAL_CODE_REQUESTED) {
     if (DEBUG_MESSAGES) {
-      Serial.write("Running original code\n");
+      DebugSerial.write("Running original code\n");
     }
     delay(100);
     while (1);
@@ -64,7 +65,7 @@ void setup() {
   RPU_SetDisableFlippers(true);
 
   if (DEBUG_MESSAGES) {
-    Serial.write("Waiting for LISY command\n");
+    DebugSerial.write("Waiting for LISY command\n");
   }
 
   LISYBytesSeen = 0;
@@ -73,8 +74,6 @@ void setup() {
   pinMode(13, OUTPUT);
   digitalWrite(13, false);
 
-  //LISYSerial.write("LISY PORT\n");
-//  RPU_SetCoinLockout((Credits >= MaximumCredits) ? true : false);
 
   for (byte count=0; count<255; count++) {
     SwitchStateNeedsToBeCleared[count] = false;
@@ -93,12 +92,14 @@ byte GetChangedSwitch() {
     if (nextSwitch==SW_SELF_TEST_SWITCH) {
       // We'll map the self-test switch as the last switch
       nextSwitch = RPU_GetSwitchCount()-1;
-      Serial.write("Self test\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Self test\n");
     }
     SwitchStateNeedsToBeCleared[nextSwitch] = true;
-    char buf[128];
-    sprintf(buf, "Switch = 0x%02X\n", nextSwitch);
-    Serial.write(buf);
+    if (DEBUG_MESSAGES) {
+      char buf[128];
+      sprintf(buf, "Switch = 0x%02X\n", nextSwitch);
+      DebugSerial.write(buf);
+    }
     return (nextSwitch | 0x80); // b7 on means that the switch is on
   } else {
     // See if there are any switch states that need to be cleared
@@ -128,7 +129,7 @@ boolean ProcessLISYMessage(byte *lisyMSG, int numBytes) {
   switch (lisyMSG[0]) {
     case 0x00:
       // Queried for architecture name
-      Serial.write("Asked for name\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked for name\n");
       LISYSerial.write("LISY 35");
       LISYSerial.write(0);    
       messageHandled = true;  
@@ -136,7 +137,7 @@ boolean ProcessLISYMessage(byte *lisyMSG, int numBytes) {
     case 0x01:
       // Queried for firmware version -- not sure how to answer, 
       // so I'm using the example from the docs
-      Serial.write("Asked for firmware\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked for firmware\n");
       LISYSerial.write("4.01");
       LISYSerial.write(0);
       messageHandled = true;
@@ -144,20 +145,20 @@ boolean ProcessLISYMessage(byte *lisyMSG, int numBytes) {
     case 0x02:
       // Queried for API Version
       // so I'm using the example from the docs
-      Serial.write("Asked for API Version\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked for API Version\n");
       LISYSerial.write("0.08");
       LISYSerial.write(0);
       messageHandled = true;
       break;
     case 0x03:
       // Queried for number of simple lamps
-      Serial.write("Asked for number of simple lamps\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked for number of simple lamps\n");
       LISYSerial.write((byte)RPU_GetLampCount());
       messageHandled = true;
       break;
     case 0x04:
       // Queried for solenoid count
-      Serial.write("Asked for solenoid count\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked for solenoid count\n");
       LISYSerial.write(RPU_GetSolenoidCount());
       messageHandled = true;
       break;
@@ -165,13 +166,13 @@ boolean ProcessLISYMessage(byte *lisyMSG, int numBytes) {
       // Queried for sound count
       // This is only returning sounds that
       // will be played on built-in hardware
-      Serial.write("Asked for sound count\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked for sound count\n");
       LISYSerial.write(RPU_GetSoundCount());
       messageHandled = true;
       break;
     case 0x06:
       // Queried for number of displays?
-      Serial.write("Asked for number of displays\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked for number of displays\n");
 #if (RPU_MPU_ARCHITECTURE==15)
       LISYSerial.write(4);
 #else      
@@ -184,7 +185,7 @@ boolean ProcessLISYMessage(byte *lisyMSG, int numBytes) {
       // for System 7 displays, because they have
       // comma digits in the 1st and 4th places, 
       // but this seems to expect every digit is the same?
-      Serial.write("Asked for details of displays\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked for details of displays\n");
 #if (RPU_MPU_ARCHITECTURE==15)
       LISYSerial.write(7);
       LISYSerial.write(4); // 14 segment, fully addressable
@@ -201,38 +202,38 @@ boolean ProcessLISYMessage(byte *lisyMSG, int numBytes) {
       break;
     case 0x08:
       // Queried for "game info" but it won't be used?
-      Serial.write("Asked for game info\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked for game info\n");
       LISYSerial.write("RPU");
       LISYSerial.write(0);
       messageHandled = true;
       break;
     case 0x09:
       // Queried for number of switches
-      Serial.write("Asked for number of switches\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked for number of switches\n");
       LISYSerial.write(RPU_GetSwitchCount());
       messageHandled = true;
       break;
     case 0x0A:
       // Queried for lamp status
-      Serial.write("Asked for lamp status\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked for lamp status\n");
       LISYSerial.write(RPU_ReadLampState(lisyMSG[1]));
       messageHandled = true;
       break;
     case 0x0B:
       // Set lamp state on
-      Serial.write("Turned lamp on\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Turned lamp on\n");
       RPU_SetLampState(lisyMSG[1], 1);
       messageHandled = true;
       break;
     case 0x0C:
       // Set lamp state off
-      Serial.write("Turned lamp off\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Turned lamp off\n");
       RPU_SetLampState(lisyMSG[1], 0);
       messageHandled = true;
       break;
     case 0x14:
       // Get solenoid status
-      Serial.write("Solenoid status queried\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Solenoid status queried\n");
       LISYSerial.write(RPU_GetSolenoidStatus(lisyMSG[1]));
       messageHandled = true;
       break;
@@ -284,7 +285,7 @@ boolean ProcessLISYMessage(byte *lisyMSG, int numBytes) {
     case 0x22:
     case 0x23:
     case 0x24:
-      Serial.write("Wrote to display\n");    
+      if (DEBUG_MESSAGES) DebugSerial.write("Wrote to display\n");    
       displayNum = lisyMSG[0] - 0x1E;
       lisyMSG[2 + lisyMSG[1]] = 0;
 #if (RPU_MPU_ARCHITECTURE<15)
@@ -308,7 +309,7 @@ boolean ProcessLISYMessage(byte *lisyMSG, int numBytes) {
       messageHandled = true;
       break;
     case 0x64:
-      Serial.write("Asked to reset\n");
+      if (DEBUG_MESSAGES) DebugSerial.write("Asked to reset\n");
       // Reset and acknowledge
       LISYSerial.write(0);
       messageHandled = true;
@@ -366,8 +367,6 @@ boolean LISYUpdate() {
     }
     
     if (LISYBytesSeen>0 && LISYBytesExpected==LISYBytesSeen) {
-//      sprintf(buf, "Message 0x%02X to be processed\n", LISYMessage[0]);
-//      Serial.write(buf);
       ProcessLISYMessage(LISYMessage, LISYBytesSeen);
       LISYBytesExpected = 0;
       LISYBytesSeen = 0;
@@ -375,10 +374,6 @@ boolean LISYUpdate() {
       LISYBytesSeen = 0;
     }
     
-//    sprintf(buf, "Saw 0x%02X\n", dat);
-//    Serial.write(buf);
-//    if (dat==0x0A) sawSomething = true;
-//    if (dat==0x64) LISYSerial.write(0);
   }
 
   return true;
@@ -400,7 +395,7 @@ void loop() {
       LastLoopReportTime = CurrentTime;
       char buf[128];
       sprintf(buf, "Loop running at %lu Hz\n", NumLoops);
-      Serial.write(buf);
+      DebugSerial.write(buf);
       NumLoops = 0;
     }
   }
